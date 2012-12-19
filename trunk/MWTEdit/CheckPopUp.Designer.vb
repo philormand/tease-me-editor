ac5101cc3a6e99c7a87c998356d4fc846abfe2af
philormand@hotmail.com <philormand@hotmail.com@82e7d143-8deb-a829-ab16-d8593f0cfb03>
2012-12-19 21:40:31 +0000

    svn add to add new files

---
+++ trunk/MWTEdit/CheckPopUp.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CheckPopUp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCheck = New System.Windows.Forms.TextBox
+        Me.btnClose = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'txtCheck
+        '
+        Me.txtCheck.Location = New System.Drawing.Point(20, 17)
+        Me.txtCheck.Multiline = True
+        Me.txtCheck.Name = "txtCheck"
+        Me.txtCheck.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtCheck.Size = New System.Drawing.Size(720, 376)
+        Me.txtCheck.TabIndex = 0
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(663, 403)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'CheckPopUp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(752, 438)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.txtCheck)
+        Me.Name = "CheckPopUp"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "CheckPopUp"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtCheck As System.Windows.Forms.TextBox
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+End Class
